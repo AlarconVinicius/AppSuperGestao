6a298ed0e23d4c777877c241729ee803797b6d25
AlarconVinicius <alarcon.pqd74@gmail.com>
2022-03-20 13:07:32 -0300

    Backup 1.0

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre_nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){ return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){ return 'Login';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Login';})->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Login';})->name('app.produtos');
 });
+
+Route::fallback(function() {
+    echo 'Página não encontrada. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial.';
+});
+
+Route::get('/teste/{p1}/{p2}', [App\Http\Controllers\TesteController::class, 'teste']);
+
+//Route::get($uri, $callback);
+/*
+verbo http:
+
+get
+post
+put
+patch
+delete
+options
+*/

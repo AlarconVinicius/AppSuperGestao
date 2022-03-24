63c0f30bbfa33e19fc57af1cfbc839e483ca8711
AlarconVinicius <alarcon.pqd74@gmail.com>
2022-03-24 11:26:14 -0300

    Views

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,7 +1,21 @@
-<h3>Sobre Nós (view)</h3>
+@extends('site.layouts.basico')
 
-<ul>
-    <li><a href="/">Principal</a></li>
-    <li><a href="/sobre_nos">Sobre Nós</a></li>
-    <li><a href="/contato">Contato</a></li>
-</ul>
+@section('titulo', 'Sobre Nós')
+
+@section('conteudo')
+    
+    <div class="conteudo-pagina">
+
+        <div class="titulo-pagina">
+            <h1>Olá, eu sou o Super Gestão</h1>
+        </div>
+
+        <div class="informacao-pagina">
+            <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
+            <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
+        </div>  
+    </div>
+
+    @include('site.layouts._partials.rodape')
+    
+@endsection
